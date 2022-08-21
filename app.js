2754f9aa4de0305395331651c6b5d3d7de5a3434
AbbosxojaMarufxojayev <abbosxojamarufxojayev07@gmail.com>
2022-08-21 19:40:55 +0500

    js

---
+++ app.js
@@ -0,0 +1,11 @@
+const sidebar = document.querySelector(".sidebar")
+const msg_container = document.querySelector(".msg_container")
+
+function showSidebar(){
+    sidebar.classList.toggle("show")
+}
+
+
+function showMsg(){
+    msg_container.classList.toggle("show_msg")
+}
